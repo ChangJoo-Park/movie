b8351daf47aac8c9eedd1d31bd0deedd697e4950
Chang Ju Park <pcjpcj2@gmail.com>
2013-05-04 03:29:11 +0900

    프로젝트용 현재 Test_Movie를 클래스를 이용해서 DB접속 테스트까지 완료

---
+++ WebContent/index.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="EUC-KR"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=EUC-KR">
+<title>Insert title here</title>
+</head>
+<body>
+hiㄴㅇㄻㄴㅇㄻㄴㅇㄹasdfasdfasdfasdfsadf
+<form method="post" action="index.jsp">
+	<div>
+		<p>이름</p>
+		<input type="text">
+	</div>
+	<input type="submit">
+</form>
+</body>
+</html>
---
+++ src/Movie/Test_JDBC_ORACLE.java
@@ -0,0 +1,60 @@
+package Movie;
+
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Test_JDBC_ORACLE {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		java.sql.Connection conn = null;
+		ResultSet rs = null;
+		Statement stmt = null;
+		String url = null;
+		String id = "hotsun0428";
+		String pwd = "rudah0428";
+/**
+ * 경모형 cafe24 데이터베이스에 있는 oracle db의 MOVIE_TEST 테이블을 이용함
+ * MOVIE_TEST
+ * title 		: String
+ * description 	: String
+ * release_year : int
+ * genre 		: String
+ * rate 		: int
+ * officialSite : String
+ * photo 		: String
+ */
+		try {
+			url = "jdbc:oracle:thin:@oracle.hotsun0428.cafe24.com:1521:orcl";
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			conn = DriverManager.getConnection(url, id, pwd);
+			
+			System.out.println("DB connection complete");
+			
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery("select * from MOVIE_TEST");
+			
+			while(rs.next()){
+				String title = rs.getString(1);
+				String description = rs.getString(2);
+				int release_year  = rs.getInt(3);
+				String genre = rs.getString(4);
+				int rate = rs.getInt(5);
+				String officialSite = rs.getString(6);
+				String photo = rs.getString(7);
+				
+				System.out.println(title+release_year+genre+officialSite+photo+rate+description);
+			}
+			
+			stmt.close();
+			conn.close();
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		
+	}
+
+}
