69db0bbaaabc027d1c0f069918c536fb14d03cdf
Chang Ju Park <pcjpcj2@gmail.com>
2013-05-04 13:42:08 +0900

    Movie 클래스 작성 /**  * title 		: String  * description 	: String  * release_year : int  * genre 		: String  * rate 		: int  * officialSite : String  * photo 		: String  */

--- src/movie/Movie.java
+++ src/movie/Movie.java
@@ -1,5 +1,61 @@
 package movie;
-
+/**
+ * title 		: String
+ * description 	: String
+ * release_year : int
+ * genre 		: String
+ * rate 		: int
+ * officialSite : String
+ * photo 		: String
+ */
 public class Movie {
-
+	private String title;
+	private String description;
+	private int release_year;
+	private String genre;
+	private int rate;
+	private String officialSite;
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public int getRelease_year() {
+		return release_year;
+	}
+	public void setRelease_year(int release_year) {
+		this.release_year = release_year;
+	}
+	public String getGenre() {
+		return genre;
+	}
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+	public int getRate() {
+		return rate;
+	}
+	public void setRate(int rate) {
+		this.rate = rate;
+	}
+	public String getOfficialSite() {
+		return officialSite;
+	}
+	public void setOfficialSite(String officialSite) {
+		this.officialSite = officialSite;
+	}
+	public String getPhoto() {
+		return photo;
+	}
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+	private String photo;
 }
