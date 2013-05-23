4e7b7cf1c6088bf7a780d9c8138e90bb0998e6e1
Chang Ju Park <pcjpcj2@gmail.com>
2013-05-23 16:33:35 +0900

    예매 레이아웃

--- WebContent/assets/js/test.js
+++ WebContent/assets/js/test.js
@@ -1,1 +1,27 @@
-alert("dd");
+$('document').ready(function(){
+});
+
+$('.reserve_movie').click(function(){
+
+});
+
+$('.radio input').click(function(){
+	name = "영화";
+	theater = "극장";
+	room ="관";
+	date ="날짜";
+	time = "시간";
+	flag = $(this).attr("name");
+	
+	if(flag==="movie"){ name = $(this).val(); $("#movie_name").text(name);}
+	else if(flag==="theater"){ theater = $(this).val(); $("#movie_theater").text(theater);}
+	else if(flag==="room"){ room = $(this).val(); $("#movie_room").text(room);}
+	else if(flag==="date"){ date = $(this).val(); $("#movie_date").text(date);}
+	else if(flag==="time"){ time = $(this).val(); $("#movie_time").text(time);}
+
+	
+	
+});
+$('.radio ').click(function(){
+	$(this).addClass("selected");
+});
